fdf15bbf824afcac9f5065db7f33f10f0fc7c5c0
Shahaan Pillay <shahaanp19@gmail.com>
2026-09-13 21:51:41 +0200

    Added telemetry ingestion integration test

--- SmartX.Shared/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SmartX.Shared
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ SmartX.Tests/Integration/TelemetryIngestionIntegrationTests.cs
@@ -0,0 +1,113 @@
+﻿using SmartX.Api.Controllers;
+using SmartX.Api.Services;
+using SmartX.Shared.DTOs;
+using SmartX.Shared.Validators;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartX.Tests.Integration;
+
+public class TelemetryIngestionIntegrationTests
+{
+    [Fact]
+    public void RegisteredSensor_ShouldSuccessfullyIngestTelemetryThroughApiPipeline()
+    {
+        // Arrange
+        var sensorRegistry =
+            new SensorRegistryService();
+
+        var thresholdValidator =
+            new TelemetryThresholdValidator();
+
+        var telemetryValidator =
+            new TelemetryValidator(
+                thresholdValidator);
+
+        var ingestionService =
+            new TelemetryIngestionService(
+                telemetryValidator);
+
+        var deploymentValidator =
+            new RecursiveDeploymentValidator();
+
+        var batchProcessor =
+            new TelemetryBatchProcessor();
+
+        var controller =
+            new TelemetryController(
+                ingestionService,
+                deploymentValidator,
+                sensorRegistry,
+                batchProcessor);
+
+        var sensorId =
+            Guid.NewGuid();
+
+        sensorRegistry.Register(
+            sensorId,
+            new
+            {
+                SensorId = sensorId,
+                Name = "Integration Test Sensor",
+                SensorType = "Environmental",
+                Location = "Hydroponic Farm A"
+            });
+
+        var request =
+            new TelemetryIngestionRequest
+            {
+                SensorDeviceId =
+                    sensorId,
+
+                MetricName =
+                    "Soil Moisture",
+
+                Unit =
+                    "%",
+
+                Value =
+                    45,
+
+                MinimumExpectedValue =
+                    20,
+
+                MaximumExpectedValue =
+                    80,
+
+                WarningThreshold =
+                    70,
+
+                CriticalThreshold =
+                    90,
+
+                IsEnabled =
+                    true
+            };
+
+        // Act
+        var result =
+            controller.Ingest(request);
+
+        // Assert
+        var okResult =
+            Assert.IsType<OkObjectResult>(
+                result.Result);
+
+        var ingestionResult =
+            Assert.IsType<TelemetryIngestionResult>(
+                okResult.Value);
+
+        Assert.True(
+            ingestionResult.Success);
+
+        Assert.False(
+            ingestionResult.IsAnomaly);
+
+        Assert.Equal(
+            sensorId,
+            ingestionResult.SensorDeviceId);
+
+        Assert.NotEqual(
+            Guid.Empty,
+            ingestionResult.PacketId);
+    }
+}
